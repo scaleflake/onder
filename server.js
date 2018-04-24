8ec73e53dd742804715ab037ec31f3496317b368
scaleflake <scaleflake@gmail.com>
2018-04-24 22:31:47 +0300

    server

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/test.html');
+});
+
+app.get('/test', function(req, res) {
+	res.sendFile(__dirname + '/test.html');
+});
+app.get('/village', function(req, res) {
+	res.sendFile(__dirname + '/village/index.html');
+});
+
+app.get('/home', function(req, res) {
+	res.sendFile(__dirname + '/gui/home.html');
+});
+
+app.get('/ip', function(req, res) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log(ip);
+	if (ip == '::ffff:127.0.0.1') {
+		console.log('localhost');
+		res.send('127.0.0.1');
+	} else if (ip == '::ffff:192.168.0.9') {
+		console.log('LAN');
+		res.send('192.168.0.49');
+	} else {
+		console.log('WAN');
+		res.send('92.255.206.247');
+	}
+	
+});
+
+app.listen(9001);
+console.log('Server started');
