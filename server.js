de9b8c0512ca28e0693b5d32e8a9de098804b8ac
scaleflake <scaleflake@gmail.com>
2018-05-02 11:36:16 +0200

    0.04

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var public = require('public-ip');
+
 var express = require('express');
 var app = express();
 
@@ -16,20 +18,20 @@ app.get('/gui', function(req, res) {
 });
 
 app.get('/ip', function(req, res) {
-	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	console.log(ip);
-	if (ip == '::ffff:127.0.0.1') {
-		console.log('localhost');
+	var reqip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	if (reqip == '::ffff:127.0.0.1') {
+		console.log('localhost : ' + ip + '\n');
 		res.send('127.0.0.1');
-	} else if (ip == '::ffff:192.168.0.9') {
-		console.log('LAN');
+	} else if (reqip.match(/::ffff:192.168.0./)) {
+		console.log('LAN : ' + reqip + ' : ' + '192.168.0.49' + '\n');
 		res.send('192.168.0.49');
 	} else {
-		console.log('WAN');
-		res.send('92.255.206.247');
+		public.v4().then(extip => {
+			console.log('WAN : ' + reqip + ' : ' + extip + '\n');
+			res.send(extip);
+		});
 	}
-	
 });
 
-app.listen(9001);
+app.listen(80);
 console.log('Server started');
